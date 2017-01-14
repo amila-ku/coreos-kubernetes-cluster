774b70c6c632ba91db5c267bd80da4fb289c8560
root <root@ubuntu-xenial.localdomain>
2017-01-14 07:36:39 +0000

    worker user

---
+++ worker.tf
@@ -0,0 +1,44 @@
+resource "aws_autoscaling_group" "worker" {
+#  vpc_zone_identifier       = ["${aws_subnet.k8-master-subnet.id}"]
+  availability_zones   = ["${split(",", var.availability_zones)}"]
+  name                      = "k8-worker-asg-${var.cluster_name}"
+  max_size                  = 3
+  min_size                  = "${var.worker_node_count}"
+  health_check_grace_period = 300
+  health_check_type         = "EC2"
+  desired_capacity          = "${var.worker_node_count}"
+  force_delete              = false
+  launch_configuration      = "${aws_launch_configuration.master.name}"
+
+  tag {
+    key                 = "Name"
+    value               = "worker-${var.cluster_name}"
+    propagate_at_launch = "true"
+  }
+  tag {
+    key                 = "apptype"
+    value               = "k8-worker"
+    propagate_at_launch = "true"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+
+resource "aws_launch_configuration" "worker" {
+  name                 = "k8-worker-lc-${var.cluster_name}"
+  image_id             = "${lookup(var.aws_amis, var.aws_region)}"
+  instance_type        = "${var.worker_ins_type}"
+  key_name             = "${var.key_name}"
+  security_groups      = ["${aws_security_group.default_kube.id}"]
+#  user_data            = "${file("${path.module}/files/master-userdata.yml")}"
+  user_data            = "${template_file.worker-user-data.rendered}"
+  iam_instance_profile = "${aws_iam_instance_profile.worker.id}"
+
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
