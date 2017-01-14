4e6cec1f0ee44f5c4273e01ff52a481bffa705a3
Kumaranayaka, Amila <Amila.Kumaranayaka@pearson.com>
2017-01-14 13:15:24 +0530

    added worker userdata template

--- template.tf
+++ template.tf
@@ -23,3 +23,21 @@ resource "template_file" "master-user-data" {
 
 }
 
+resource "template_file" "worker-user-data" {
+  template = "${file("data/master-userdata.yml")}"
+  vars  {
+    KUBERNETES_VERSION = "${var.kubernetes_version}"
+    KUBERNETES_CONTAINERS_CIDR = "${var.pod_network}"
+    FLANNELD_ETCD_ENDPOINTS = "http://${aws_elb.etcd-elb.dns_name}:2379"
+    ETCD_ENDPOINTS = "http://${aws_elb.etcd-elb.dns_name}:2379"
+    SERVICE_IP_RANGE = "${var.service_ip_range}"
+    API_SECURE_PORT = "${var.api_secure_port}"
+    DNS_SERVICE_IP = "${var.dns_service_ip}"
+    MASTER_ELB = "${aws_elb.master-elb.dns_name}"
+    S3_BUCKET = "${var.s3_bucket}"
+    K8S_VER = "${var.kubelet_version}"
+    aws_region = "${var.aws_region}"
+  }
+
+}
+
